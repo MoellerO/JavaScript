cff11e67aac388655c60be5466ed05fd1f42b752
MoellerO <oliver.moeller@die-olchies.de>
2022-02-15 18:32:24 +0100

    DM grid traveller

---
+++ dynamic_programming/grid_traveller.js
@@ -0,0 +1,17 @@
+//compute number of ways travelling through a grid
+const travel = (x, y, memo = {}) => {
+  if (x == 0 || y == 0) return 0
+  if (x == 1 || y == 1) return 1
+  if (x in memo) {
+    if (y in memo[x]) {
+      return memo[x][y]
+    }
+  } else {
+    memo[x] = {}
+  }
+  memo[x][y] = travel(x - 1, y, memo) + travel(x, y - 1, memo)
+  return memo[x][y]
+}
+
+
+console.log(travel(18, 18))
